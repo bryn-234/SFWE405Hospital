e854f636703af379d3cdc1400fb2a5ca9e7a2378
Miguel Sena <520migs@gmail.com>
2026-02-20 23:32:53 -0700

    Refactor AppointmentRepository to interface with JPA

--- src/main/java/SFWE405/project/code/Repositories/AppointmentRepository.java
+++ src/main/java/SFWE405/project/code/Repositories/AppointmentRepository.java
@@ -1,5 +1,12 @@
 package SFWE405.project.code.Repositories;
 
-public class AppointmentRepository {
-    
+import SFWE405.project.code.Entities.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+@Repository
+public interface AppointmentRepository {
+    List<Appointment> findByPatientId(Long patientId);
+    List<Appointment> findbyDoctorId(Long doctorId);
 }
