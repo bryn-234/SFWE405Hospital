dd96c12890ca456b291e4df6523f99698a49cf1e
CharlatteMontague <166445014+CharlatteMontague@users.noreply.github.com>
2026-02-21 12:16:06 -0700

    Update Doctor.java
    Created Doctor entity with id and name attributes and Department and Appointment associations

--- src/main/java/SFWE405/project/code/Entities/Doctor.java
+++ src/main/java/SFWE405/project/code/Entities/Doctor.java
@@ -1,4 +1,32 @@
 package SFWE405.project.code.Entities;
+
+import java.util.Set;
+
+import jakarta.persistence.*;
+
 public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany
+    private Set<Appointment> appointments;
     
+    @ManyToOne
+    private Department department;
+
+    public Doctor(){}
+
+    public Long getId() {return id;}
+    
+    public void setName(String name) {this.name = "Dr." + name;}
+    public String getName() {return name;}
+
+    public void addAppointment(Appointment a) {this.appointments.add(a);}
+    public Set<Appointment> getAppointments() {return appointments;}
+
+    public void addDepartment(Department d) {this.department = d;}
+    public Department getDepartment() {return department;}
 }
