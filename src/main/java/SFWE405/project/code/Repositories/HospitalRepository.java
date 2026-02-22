6ea33f73ecf7485ccf7f2b639f401d216a52db88
afsah24 <161001938+afsah24@users.noreply.github.com>
2026-02-21 17:24:37 -0700

    Refactor HospitalRepository to extend JpaRepository

--- src/main/java/SFWE405/project/code/Repositories/HospitalRepository.java
+++ src/main/java/SFWE405/project/code/Repositories/HospitalRepository.java
@@ -1,5 +1,10 @@
 package SFWE405.project.code.Repositories;
 
-public class HospitalRepository {
-    
+import SFWE405.project.code.Entities.Hospital;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface HospitalRepository extends JpaRepository<Hospital, Long> {
+    Hospital findByName(String name);
 }
