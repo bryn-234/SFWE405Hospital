0a874ffe047e5cab973e39f07094f1b80b50329c
afsah24 <161001938+afsah24@users.noreply.github.com>
2026-02-21 17:24:08 -0700

    Refactor InsuranceRepository to extend JpaRepository

--- src/main/java/SFWE405/project/code/Repositories/InsuranceRepository.java
+++ src/main/java/SFWE405/project/code/Repositories/InsuranceRepository.java
@@ -1,5 +1,12 @@
 package SFWE405.project.code.Repositories;
 
-public class InsuranceRepository {
-    
+import SFWE405.project.code.Entities.Insurance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InsuranceRepository extends JpaRepository<Insurance, Long> {
+    List<Insurance> findByProviderName(String providerName);
+    Insurance findByPolicyNumber(String policyNumber);
+    Insurance findByPatient_Ssn(Long ssn);
 }
