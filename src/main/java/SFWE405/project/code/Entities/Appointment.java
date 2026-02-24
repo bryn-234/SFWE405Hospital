9a1c19812e7104a0ad099159c1fe66b68f9c51ca
Joseph <josephc8464@gmail.com>
2026-02-23 21:24:18 -0700

    Edited entity to reduce boilerplate code

--- src/main/java/SFWE405/project/code/Entities/Appointment.java
+++ src/main/java/SFWE405/project/code/Entities/Appointment.java
@@ -1,13 +1,22 @@
 package SFWE405.project.code.Entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Appointment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,32 +42,5 @@ public class Appointment {
     @JsonBackReference
     @ManyToOne
     @JoinColumn(name = "patient_id")
-    private Patient patient;
-
-    //Constructors
-    public Appointment() {}
-
-    //Getters and Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public LocalDate getAppointmentDate() { return appointmentDate; }
-    public void setAppointmentDate(LocalDate appointmentDate) {this.appointmentDate = appointmentDate; }
-    public String getReasonForVisit() { return reasonForVisit; }
-    public void setReasonForVisit(String reasonForVisit) {this.reasonForVisit = reasonForVisit;}
-    public String getStatus() { return status; }
-    public void setStatus(String status) {this.status = status; }
-    public int getCost() {return cost;}
-    public void setCost(int cost) {this.cost = cost;}
-    public int getRoomNum() {return roomNum;}
-    public void setRoomNum(int roomNum) {this.roomNum = roomNum;}
-    public Patient getPatient() { return patient; }
-    public void setPatient(Patient patient) { this.patient = patient; }
-    public Doctor getDoctor() { return doctor; }
-    public void setDoctor(Doctor doctor) { this.doctor = doctor; }
-   
-    //I commented this out to match the domain diagram we currently have
-
-    //public Department getDepartment() { return department; }
-    //public void setDepartment(Department department) { this.department = department; }
-                                               
+    private Patient patient;                   
 }
