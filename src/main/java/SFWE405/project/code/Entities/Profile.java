dc5ca5d0f1fdd2b841190280481802a6c7f0f8c1
CharlatteMontague <166445014+CharlatteMontague@users.noreply.github.com>
2026-03-18 16:26:07 -0700

    Update and rename MedicalRecord.java to Profile.java

---
+++ src/main/java/SFWE405/project/code/Entities/Profile.java
@@ -0,0 +1,30 @@
+package SFWE405.project.code.Entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+
+    @OneToOne
+    private Doctor doctor;
+
+    @OneToOne
+    private Patient patient;
+
+
+}
