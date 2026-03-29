dcc379977e9f26b7a6ac1fee9ae5be3996719474
CharlatteMontague <166445014+CharlatteMontague@users.noreply.github.com>
2026-03-28 22:12:37 -0700

    Update AppointmentService.java
    Added functionality for requirements 3.1-3.4

--- src/main/java/SFWE405/project/code/Services/AppointmentService.java
+++ src/main/java/SFWE405/project/code/Services/AppointmentService.java
@@ -12,10 +12,24 @@
 
 package SFWE405.project.code.Services;
 
+import SFWE405.project.code.InsufficientInfoException;
+import SFWE405.project.code.OccupancyMetException;
+import SFWE405.project.code.TimeSlotTakenException;
 import SFWE405.project.code.Entities.Appointment;
 import SFWE405.project.code.Entities.Hospital;
+import SFWE405.project.code.Entities.Patient;
+import SFWE405.project.code.Entities.Schedule;
+import SFWE405.project.code.Entities.TimeSlot;
+import SFWE405.project.code.Entities.Doctor;
 import SFWE405.project.code.Repositories.AppointmentRepository;
+import SFWE405.project.code.Repositories.DoctorRespository;
 import SFWE405.project.code.Repositories.HospitalRepository;
+import SFWE405.project.code.Repositories.TimeSlotRepository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.InputMismatchException;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,11 +37,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class AppointmentService {
     private final AppointmentRepository appointmentRepository;
     private final HospitalRepository hospitalRepository;
+    private final DoctorRespository doctorRepo;
+    private final TimeSlotRepository TimeslotRepo;
 
     public AppointmentService(AppointmentRepository appointmentRepository,
-                              HospitalRepository hospitalRepository) {
+                              HospitalRepository hospitalRepository, DoctorRespository doctorRepo, TimeSlotRepository TimeslotRepo) {
         this.appointmentRepository = appointmentRepository;
         this.hospitalRepository = hospitalRepository;
+        this.doctorRepo = doctorRepo;
+        this.TimeslotRepo = TimeslotRepo;
     }
 
     /**
@@ -72,5 +90,72 @@ public class AppointmentService {
         return appointment;
     }
 
+    /**
+     * Checks that an Appointment mets the criteria before sceduling it.
+     *
+     * This method enforces the following requirements:
+     * 3.1: The system shall reject an appointment request if the Hospital occupancy has reached its maximum capacity.
+     * 3.2: The system shall not allow an appointment to be created without a specified doctor, patient, and department.
+     * 3.3: The system shall only allow an appointment to be made with a doctor belonging to the department specified by the patient.
+     * 3.4: The system shall prevent an appointment from being scheduled if the selected time slot in the Schedule is not marked as "Open." 
+     * @return the new Appointment object
+     * @throws OccupancyMetException if the hospitals occupancy has been met
+     * @throws InsufficientInfoException if the patient, doctor, or department is missing
+     * @throws TimeSlotTakenException if the time slot chosen has been taken
+     * Author: Charlotte Montague
+     */
+    
+    @Transactional
+    public Appointment schedule(Appointment app, Long id) throws OccupancyMetException, InsufficientInfoException, TimeSlotTakenException{
+        Hospital hospital = hospitalRepository.findById(id).orElseThrow(() -> new RuntimeException("Hospital not found"));
+
+        //Implement Requirement 3.1
+        //The system shall reject an appointment request if the Hospital occupancy has reached its maximum capacity.
+        checkOccupancy(hospital);
+
+        //Implement Requirement 3.2
+        //The system shall not allow an appointment to be created without a specified doctor, patient, and department.
+        checkSufficientInfo(app);
+
+        //Implement Requirement 3.3
+        //The system shall only allow an appointment to be made with a doctor belonging to the department specified by the patient.
+        Doctor doctor = doctorRepo.findById(app.getDoctor().getId()).orElseThrow(() -> new RuntimeException("Doctor not found"));
+        checkDepartment(doctor, app);
+
+        //Implement Requirement 3.4
+        //The system shall prevent an appointment from being scheduled if the selected time slot in the Schedule is not marked as "Open."
+        TimeSlot ts = TimeslotRepo.findById(app.getTimeslot().getId()).orElseThrow(() -> new RuntimeException("Time slot not found"));
+        checkTimeSlot(ts);
+
+        return appointmentRepository.save(app);
+    } 
+
+    //checks if the Hospitals Occupancy is equal to its capacity
+    public void checkOccupancy(Hospital hospital) throws OccupancyMetException{
+        if(hospital.getOccupancy() == hospital.getCapacity()){
+            throw new OccupancyMetException("Hospital Occupency Met");
+        }
+    }
+
+    //checks a doctor, patient, and department are specified
+    public void checkSufficientInfo(Appointment app) throws InsufficientInfoException{
+        if(app.getDoctor() == null || app.getPatient() == null || app.getDepartment() == null){
+            throw new InsufficientInfoException("Either Doctor, Patient, or Department wasn't specified.");
+        }
+    }
+
+    //checks that the doctor's department is equal to the department specified
+    public void checkDepartment(Doctor d, Appointment app){
+        if(!d.getDepartment().getId().equals(app.getDepartment().getId())){
+            throw new InputMismatchException("Chosen Doctor's department doesn't match one chosen.");
+        }
+    }
+
+    //checks that the time slot chosen is open
+    public void checkTimeSlot(TimeSlot ts) throws TimeSlotTakenException{
+        if(!ts.getAvailable()){
+            throw new TimeSlotTakenException("Chosen Time Slot is unavailable");
+        }
+    }
 
 }
