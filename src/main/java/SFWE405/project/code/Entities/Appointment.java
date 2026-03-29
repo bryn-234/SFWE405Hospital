15e7dde21ee8b0ba4a528405d0e3a49b4497ecc6
CharlatteMontague <166445014+CharlatteMontague@users.noreply.github.com>
2026-03-28 22:01:45 -0700

    Update Appointment.java
    added department and timeslot associations

--- src/main/java/SFWE405/project/code/Entities/Appointment.java
+++ src/main/java/SFWE405/project/code/Entities/Appointment.java
@@ -8,6 +8,8 @@ import lombok.Setter;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Getter
 @Setter
@@ -21,20 +23,25 @@ public class Appointment {
     private LocalDate appointmentDate;
     private String reasonForVisit;
     private String status;
-    private int cost;
-    private int roomNum;
+    private Integer cost;
+    private Integer roomNum;
 
     @ManyToOne
     @JoinColumn(name = "doctor_id")
     private Doctor doctor;
 
-    //I commented this out to match the domain diagram we currently have. 
-    //We can always add it back in later if we decide to include it.
-    /*@ManyToOne
+    //Need this for requirements 3.2 and 3.3
+    @ManyToOne
     @JoinColumn(name = "department_id")
-    private Department department;*/
+    private Department department;
 
     @ManyToOne
     @JoinColumn(name = "patient_id")
-    private Patient patient;                   
+    private Patient patient; 
+
+    //Needed for requirement 3.4
+    @OneToOne
+    @JoinColumn(name = "timeslot_id")
+    @JsonIgnoreProperties("appointment")
+    private TimeSlot timeslot;
 }
